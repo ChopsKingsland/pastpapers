eb17fbdfa508b4a45359590e68540696bb941207
Charlie Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2022-03-31 20:48:15 +0100

    papers list

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
-// const jsonData = require('subjects.json');
+$.getJSON("https://raw.githubusercontent.com/ChopsKingsland/pastpapers/gh-pages/subjects.json", function (data) {
+  var list = document.getElementById('papers');
 
-fetch("./subjects.json")
-.then(response => {
-    var meJSON = response.json();
-    document.getElementById('test').innerHTML = meJSON['biology-8461'];
-//    return response.json();
-})
-// .then(jsondata => console.log(jsondata));
+  for (const subj in data) {
+    var li = document.createElement("li");
+    li.insertAdjacentHTML("beforeend", `<br><h1>${subj}</h1>`);
+    list.appendChild(li);
+    for (const year in data[subj]) {
+        for (const paper in data[subj][year]) {
+            // console.log(data[subj][year][paper]);
+            var papername = paper
+            var paperURL = data[subj][year][paper]
+            // console.log(`<a href="${paperURL}">${papername}</a>`);
+            var li = document.createElement("li");
+            li.insertAdjacentHTML("beforeend", `<a href="${paperURL}">${papername}</a>`);
+            list.appendChild(li);
+        }
+    }
+  }
 
+});
