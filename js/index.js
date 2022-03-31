fb466a46805ab27f7967fcbd9e9d6fedd13d2386
Charlie Kingsland <18499854+ChopsKingsland@users.noreply.github.com>
2022-03-31 21:03:55 +0100

    formatting

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 $.getJSON("https://raw.githubusercontent.com/ChopsKingsland/pastpapers/gh-pages/subjects.json", function (data) {
   var list = document.getElementById('papers');
 
   for (const subj in data) {
     var li = document.createElement("li");
-    li.insertAdjacentHTML("beforeend", `<br><h1>${subj}</h1>`);
+    li.insertAdjacentHTML("beforeend", `<br><br><h1>${capitalizeFirstLetter(subj.replace('-', ' '))}</h1>`);
     list.appendChild(li);
     for (const year in data[subj]) {
+        var li = document.createElement("li");
+        li.insertAdjacentHTML("beforeend", `<br><h2>${capitalizeFirstLetter(year.replace('-', ' '))}</h2>`);
+        list.appendChild(li);
         for (const paper in data[subj][year]) {
             // console.log(data[subj][year][paper]);
             var papername = paper
